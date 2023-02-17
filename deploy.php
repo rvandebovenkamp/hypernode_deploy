c5e8c28a98f9592f5924978a496df2cf3582509c
Rolf van de Bovenkamp <rolf@Rolfs-MBP.localdomain>
2023-02-17 10:53:45 +0100

    yml files

--- deploy.php
+++ deploy.php
@@ -1,32 +1,10 @@
 <?php
+
 namespace Hypernode\DeployConfiguration;
 
-$configuration = new ApplicationTemplate\Magento2(
- // Your GIT repository url
- 'git@github.com:rvandebovenkamp/hypernode_deploy.git',
- // Frontend locales
- ['nl_NL'],
- // Backend locales
- ['nl_NL']
-);
+$configuration = new ApplicationTemplate\Magento2(['en_US']);
 
-$stagingStage = $configuration->addStage('staging', 'magento2.testhipex.nl');
-$stagingStage->addServer('hntestrolf.hypernode.io');
-
-$configuration->setSharedFiles([
-    'app/etc/env.php',
-    'pub/errors/local.xml'
-]);
-$configuration->setSharedFolders([
-    'var/log',
-    'var/session',
-    'var/report',
-    'pub/media'
-]);
-
-$configuration->addDeployCommand(new Command\Deploy\Magento2\MaintenanceMode());
-$configuration->addDeployCommand(new Command\Deploy\Magento2\SetupUpgrade());
-$configuration->addDeployCommand(new Command\Deploy\Magento2\CacheFlush());
-
+$productionStage = $configuration->addStage('staging', 'magento2.testhipex.nl');
+$productionStage->addServer('hntestrolf.hypernode.io');
 
 return $configuration;
